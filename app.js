f9d862a733cb890fe969a76b2800da0eaaa3f42f
claudegose <84382636+claudegose@users.noreply.github.com>
2021-07-01 18:27:29 +0200

    Completed Search Bar with corresponind JS and Styling. Operational

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ setInterval(() =>{
     const ampm = hour >= 12 ? 'PM' : 'AM'
 
 
+
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) +  ':' + (minutes < 10 ? '0' +minutes: minutes) + ' ' + `<span id="am-pm">${ampm}</span>`
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 }, 1000);
